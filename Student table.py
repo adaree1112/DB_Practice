268f31947ddfd4cb7f209208e945010ec0a84ee2
adaree1112 <adaree1112@highgateschool.org.uk>
2025-01-20 15:04:47 +0000

    20/1/25

--- Student table.py
+++ Student table.py
@@ -3,7 +3,7 @@ import sqlite3
 conn=sqlite3.connect("student.sqlite")
 cursor = conn.cursor()
 create_student_table= """
-CREATE TABLE IF NOT EXISTS students(
+CREATE TABLE students(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 first_name TEXT NOT NULL,
 last_name TEXT NOT NULL,
--- Write_to_student_database.py
+++ Write_to_student_database.py
@@ -1,5 +1,8 @@
 import sqlite3
+import faker
+import random
 
+fake = faker.Faker('en_GB')
 
 with sqlite3.connect("student.sqlite") as conn:
     cursor=conn.cursor()
@@ -14,6 +17,11 @@ with sqlite3.connect("student.sqlite") as conn:
     values (?,?,?,?);
     """
 
+    fake.random.seed(4321)
+    random.seed(4321)
+    for _ in range(100):
+        f=fake.profile()
+        cursor.execute(parameterised_insert_query, [*f['name'].split(' ')[-2:], random.randint(11,40), f['sex']])
 
-    cursor.execute(parameterised_insert_query, ("Harry", "Potter", 13, "Male"))
+    # cursor.execute(parameterised_insert_query, ("Harry", "Potter", 13, "Male"))
     conn.commit()
