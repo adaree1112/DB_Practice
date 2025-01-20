30e46d0c9a58b1441ec7046ddd7420d07d98d9ab
adam <adaree1112@highgateschool.org.uk>
2025-01-20 20:57:38 +0000

    Homework 20/1/25

---
+++ sm_app/Create sm_app.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+with sqlite3.connect('sm_app.sqlite') as conn:
+    cur = conn.cursor()
+
+    create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      age INTEGER,
+      gender TEXT,
+      nationality TEXT
+    );
+    """
+
+    create_posts_table = """
+    CREATE TABLE IF NOT EXISTS posts(
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      title TEXT NOT NULL, 
+      description TEXT NOT NULL, 
+      user_id INTEGER NOT NULL, 
+      FOREIGN KEY (user_id) REFERENCES users (id)
+    );
+    """
+
+    create_likes_table = """
+    CREATE TABLE IF NOT EXISTS likes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      user_id INTEGER NOT NULL, 
+      post_id integer NOT NULL, 
+      FOREIGN KEY (user_id) REFERENCES users (id),
+      FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+    """
+
+    create_comments_table = """
+    CREATE TABLE IF NOT EXISTS comments (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      text TEXT NOT NULL, 
+      user_id INTEGER NOT NULL, 
+      post_id INTEGER NOT NULL, 
+      FOREIGN KEY (user_id) REFERENCES users (id)
+      FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+    """
+
+    cur.execute(create_users_table)
+    cur.execute(create_posts_table)
+    cur.execute(create_likes_table)
+    cur.execute(create_comments_table)
+
+    conn.commit()
