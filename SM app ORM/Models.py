666ab4ecb0cc61eabc4e05cc249abc66c5d40415
adaree1112 <adaree1112@highgateschool.org.uk>
2025-02-24 15:07:07 +0000

    not initial

--- SM app ORM/Models.py
+++ SM app ORM/Models.py
@@ -17,16 +17,19 @@ like = sa.Table('Likes',
 class User(Base):
     __tablename__ = 'users'
     id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
-    name: so.Mapped[str]
+    name: so.Mapped[str] = so.mapped_column(unique=True)
     age: so.Mapped[int]
-    gender: so.Mapped[Optional[str]]
-    nationality: so.Mapped[Optional[str]]
+    gender: so.Mapped[str]
+    nationality: so.Mapped[str]
     posts: so.Mapped[list["Post"]] = so.relationship("Post",back_populates="user")
     liked_posts: so.Mapped[list["Post"]] = so.relationship("Post",
                                                            secondary=like,
                                                            order_by='Post.title',
                                                            back_populates="liked_by_user")
-    #comments
+    comments_made: so.Mapped[list['Comment']] = so.relationship(back_populates='user')
+
+    def __repr__(self):
+        return f"User(name='{self.name}', age={self.age}, gender='{self.gender}', nationality='{self.nationality}')"
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -41,12 +44,19 @@ class Post(Base):
                                                            order_by='User.name',
                                                            back_populates="liked_posts",
                                                            )
-    #comments
-#
-# class Comment:
-#     __tablename__ = 'comments'
-#     id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
-#     user_id: sa.ForeignKey(User.id)
-#     post_id: sa.ForeignKey(Post.id)
-#     comment: so.Mapped[str]
-#     #post
+    comments: so.Mapped[list["Comment"]] = so.relationship(back_populates='post')
+
+    def __repr__(self):
+        return f"Post(title='{self.title}', description='{self.description}', user_id={self.user_id})"
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('users.id'))
+    post_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('posts.id'))
+    comment: so.Mapped[str]
+    post: so.Mapped['Post'] = so.relationship(back_populates='comments')
+    user: so.Mapped['User'] = so.relationship(back_populates='comments_made')
+
+    def __repr__(self):
+        return f"Comment(user_id={self.user_id}, post_id={self.post_id}, comment='{self.comment}')"
---
+++ SM app ORM/read.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine,select
+from sqlalchemy.orm import Session
+from Models import User, Post, Comment, Base
+
+engine = create_engine('sqlite:///Sm_app.sqlite')
+session = Session(bind=engine)
+
+# Query and print all users
+users = session.scalars(select(User)).all()
+for user in users:
+    print(user)
+
+# Query and print all posts
+posts = session.scalars(select(Post)).all()
+for post in posts:
+    print(post)
+
+# Query and print all comments
+comments = session.scalars(select(Comment)).all()
+for comment in comments:
+    print(comment)
