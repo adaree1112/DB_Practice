e1d865babf89bf20f993d6434483d544e85e7800
adaree1112 <adaree1112@highgateschool.org.uk>
2025-03-10 15:58:51 +0000

    10/3/25

--- SM app ORM/db test.py
+++ SM app ORM/db test.py
@@ -17,6 +17,7 @@ class TestDatabase:
         Base.metadata.create_all(engine)
         session = so.Session(engine)
         yield session
+        session.rollback()
         session.close()
         Base.metadata.drop_all(engine)
 
@@ -32,3 +33,35 @@ class TestDatabase:
         assert rayhan.age == 20
         assert rayhan.gender == "Male"
         assert rayhan.nationality is None
+
+    def test_invalid_user(self, db_session):
+        user = User(age=7, nationality="British")
+        db_session.add(user)
+        with pytest.raises(IntegrityError):
+            db_session.commit()
+
+        db_session.rollback()
+
+    def test_valid_post(self, db_session):
+        """ creating a user and add in a post to that user
+        - check post is included in user.posts and the user is included it post.user"""
+
+        user = User(name="Adam", age=20, gender="Male")
+
+        post = Post(title="HI", description="The Game")
+        user.posts.append(post)
+        db_session.add(user)
+        db_session.commit()
+        qry=sa.select(User).where(User.name == "Adam")
+        adam = db_session.scalar(qry)
+
+        assert adam.posts == [post]
+        assert adam.posts[0].description == "The Game"
+        assert adam.posts[0].title == "HI"
+
+        qry=sa.select(Post).where(Post.title == "HI")
+        hi = db_session.scalar(qry)
+
+        assert hi.user == user
+        assert hi.user.name == "Adam"
+        assert hi.user.age == 20
