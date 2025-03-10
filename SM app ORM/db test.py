1fe61574653b22598a8a438291f56e49c83672bf
adam <adaree1112@highgateschool.org.uk>
2025-03-10 18:53:45 +0000

    10/3/25 HW

--- SM app ORM/db test.py
+++ SM app ORM/db test.py
@@ -3,12 +3,9 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from sqlalchemy.exc import IntegrityError
 
-from models import User, Post, Comment, Base
+from models import User, Post, Comment, Base, like
 
 test_db_location = "sqlite:///:memory:"
-
-def test_test():
-    assert 3**2 ==9
 
 class TestDatabase:
     @pytest.fixture(scope="class")
@@ -25,7 +22,7 @@ class TestDatabase:
         user = User(name="Rayhan", age=20, gender="Male")
         db_session.add(user)
         db_session.commit()
-        qry=sa.select(User).where(User.name == "Rayhan")
+        qry = sa.select(User).where(User.name == "Rayhan")
         rayhan = db_session.scalar(qry)
 
         assert rayhan is not None
@@ -39,29 +36,73 @@ class TestDatabase:
         db_session.add(user)
         with pytest.raises(IntegrityError):
             db_session.commit()
-
         db_session.rollback()
 
     def test_valid_post(self, db_session):
-        """ creating a user and add in a post to that user
-        - check post is included in user.posts and the user is included it post.user"""
-
         user = User(name="Adam", age=20, gender="Male")
-
         post = Post(title="HI", description="The Game")
         user.posts.append(post)
         db_session.add(user)
         db_session.commit()
-        qry=sa.select(User).where(User.name == "Adam")
+
+        qry = sa.select(User).where(User.name == "Adam")
         adam = db_session.scalar(qry)
 
         assert adam.posts == [post]
         assert adam.posts[0].description == "The Game"
         assert adam.posts[0].title == "HI"
 
-        qry=sa.select(Post).where(Post.title == "HI")
+        qry = sa.select(Post).where(Post.title == "HI")
         hi = db_session.scalar(qry)
 
         assert hi.user == user
         assert hi.user.name == "Adam"
         assert hi.user.age == 20
+
+    def test_user_can_comment_on_post(self, db_session):
+        user = User(name="John", age=25, gender="Male")
+        post = Post(title="Test Post", description="Testing comments")
+        db_session.add_all([user, post])
+        db_session.commit()
+
+        comment = Comment(user_id=user.id, post_id=post.id, comment="Nice post!")
+        db_session.add(comment)
+        db_session.commit()
+
+        qry = sa.select(Comment).where(Comment.user_id == user.id, Comment.post_id == post.id)
+        fetched_comment = db_session.scalar(qry)
+
+        assert fetched_comment is not None
+        assert fetched_comment.comment == "Nice post!"
+        assert fetched_comment.user == user
+        assert fetched_comment.post == post
+
+    def test_user_can_like_post(self, db_session):
+        user = User(name="Alice", age=30, gender="Female")
+        post = Post(title="Like Test", description="Testing likes")
+        db_session.add_all([user, post])
+        db_session.commit()
+
+        user.liked_posts.append(post)
+        db_session.commit()
+
+        qry = sa.select(User).where(User.name == "Alice")
+        alice = db_session.scalar(qry)
+
+        assert post in alice.liked_posts
+        assert alice in post.liked_by_user
+
+    def test_user_cannot_like_post_twice(self, db_session):
+        user = User(name="Bob", age=22, gender="Male")
+        post = Post(title="Duplicate Like Test", description="Testing duplicate likes")
+        db_session.add_all([user, post])
+        db_session.commit()
+
+        user.liked_posts.append(post)
+        db_session.commit()
+
+        user.liked_posts.append(post)
+        db_session.add(user)
+        with pytest.raises(IntegrityError):
+            db_session.commit()
+        db_session.rollback()
